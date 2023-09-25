d1a69acf1ae35aa8393ab2ef5320ee9198cbba70
cmw1017 <cmw1017@naver.com>
2023-09-25 22:19:10 +0900

    230925 Vue 강의 내용 정리

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@
   <ch_02_05/>
 
   <h1> ch_03_05 실습 (computed)</h1>
+  <p> computed 변수 중 data 선언하지 않은 변수들은 수정하려고 하면 오류가 발생함</p>
+  <p> computed 변수는 기존에 선언된 변수에 추가로 계산을 하거나 속성을 추가하는 것이므로 computed를 통해 생성된 속성은 수정되지 않는 것으로 보임</p>
   <ch_03_05/>
 
   <h1> ch_03_11~12 실습 (v-if, v-show)</h1>
@@ -14,9 +16,12 @@
 
   <h1> ch_03_13 실습 (v-for)</h1>
   <p> v-for의 리스트에 반응성을 넣으려면 push, pop 등의 배열 함수를 사용하여 값을 넣어야함</p>
-  <p> 배열 요소(array[i] 방식)로 직접 할당하는 방식은 반응성을 갖지 않음</p>
+  <p> 배열 요소(array[i] 방식)로 직접 할당하는 방식은 반응성을 갖지 않음(Vue3에서는 반응성을 갖을 수 있게 수정됨)</p>
   <ch_03_13/>
 
+  <h1> ch_03_15 실습</h1>
+  <p> Vue3에서는 set을 통한 수정을 하지 않고, 데이터의 추가 또는 삭제도 감지할 수 있다고 함</p>
+  <ch_03_15/>
 </template>
 
 <script>
@@ -25,6 +30,7 @@ import ch_02_05 from './components/ch_02_05.vue'
 import ch_03_05 from './components/ch_03_05.vue'
 import ch_03_11_12 from './components/ch_03_11_12.vue'
 import ch_03_13 from './components/ch_03_13.vue'
+import ch_03_15 from './components/ch_03_15.vue'
 
 export default {
   name: 'App',
@@ -33,7 +39,8 @@ export default {
     ch_02_05,
     ch_03_05,
     ch_03_11_12,
-    ch_03_13
+    ch_03_13,
+    ch_03_15
   }
 }
 </script>
--- src/components/ch_03_15.vue
+++ src/components/ch_03_15.vue
@@ -1,18 +1,25 @@
 <template>
-     <button v-on:click="pushTodo">간식 먹기 추가</button>
+    <button v-on:click="pushTodo">간식 먹기 추가</button>
     <ul>
         <!-- 만약 id가 없다면 index로 key를 할당할 수 있음 -->
         <li v-for="(todo, index) in todos" :key="index">
             <span :class="{ done: todo.done}"> {{todo.title}} </span>
         </li>
-        
+    </ul>
+
+    <button v-on:click="addEmail">이메일 추가</button>
+    <ul>
+        <!-- 만약 id가 없다면 index로 key를 할당할 수 있음, 배열이 아닌 객체는 중간에 key를 넣어줘야 함 -->
+        <li v-for="(value, key, index) in person" :key="index">
+            <span> {{value}} </span>
+        </li>
     </ul>
 </template>
 
 <script>
 
 export default {
-    name: 'ch_03_13',
+    name: 'ch_03_15',
     data() {
         return{
             todos: [
@@ -25,15 +32,20 @@ export default {
                 {
                     title: '저녁 먹기'
                 }
-            ]
+            ],
+            person: {
+                name: "person",
+                age: 30
+            }
         }
     },
     methods: {
       pushTodo () {
-        // 아래와 같은 방식의 배열 추가는 반응성이 없음(Vue3에서는 가능)
-        // this.todos[3] = {title:'간식 먹기'}
-        this.todos.push({title:'간식 먹기'})
-      }  
+        this.todos[3] = {title:'간식 먹기'}
+      },
+      addEmail () {
+          this.person.email = "cmw1017@naver.com"
+      }
     },
     
 }
