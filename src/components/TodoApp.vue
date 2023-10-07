009d3dbdebd458012b655aa9a5a0287233bdcba7
cmw1017 <cmw1017@naver.com>
2023-10-07 16:10:22 +0900

    231007 Vue 강의 내용 정리6

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,20 +1,65 @@
 <template>
-  <div>
-    <todo-item 
-      v-for="todo in todos"
-      :key="todo.id"
-      :todo_props="todo"
-      @update-todo="updateTodo"
-      @delete-todo="deleteTodo"
-    />
+  <div class="todo-app">
+    <div class="todo-app__actions">
+      <div class="filters">
+        <!-- :class는 filter가 'all'이면 active라는 클래스를 button 요소에 삽입한다는 의미 -->
+        <!-- @click는 button 요소에 이벤트 메소드를 넣는 것 -->
+        <button
+          :class="{ active: filter === 'all' }"
+          @click="changeFilter('all')"
+        >
+          모든 항목 ({{ total }})
+        </button>
+        <button
+          :class="{ active: filter === 'active' }"
+          @click="changeFilter('active')"
+        >
+          해야 할 항목 ({{ activeCount }})
+        </button>
+        <button
+          :class="{ active: filter === 'completed' }"
+          @click="changeFilter('completed')"
+        >
+          완료된 항목 ({{ completedCount }})
+        </button>
+      </div>
 
-    <hr/>
-    <todo-creator @create-todo="createTodo" />
+      <div class="actions">
+        <!-- v-model은 input의 값과 allDone이라는 Computed 값이 양방향 바인딩이 되도록하는 것 -->
+        <input v-model="allDone" type="checkbox" />
+        <button @click="clearCompleted">완료된 항목 삭제</button>
+      </div>
+    </div>
+
+    <div class="todo-app__list">
+      <!-- 
+      v-for는 반복하는 중에 :key에 유니크한 값을 넣어줘야함
+      강의에는 :todo로 넘겨주나 여기에서는 :todo_props로 넘겨줌(자식 컴포넌트에서 todo로 하면 변수 미정의 오류가 발생함)
+      @update-todo, @delete-todo는 해당 todoItem 컴포넌트에서 $emit으로 보낸 이벤트를 받는것(이름이 이벤트 종류를 가림)
+      -->
+      <todo-item
+        v-for="todo in filteredTodos"
+        :key="todo.id"
+        :todo_props="todo"
+        @update-todo="updateTodo"
+        @delete-todo="deleteTodo"
+      />
+    </div>
+
+    <hr />
+
+    <div class="todo-app__creater">
+      <todo-creator @create-todo="createTodo" />
+    </div>
   </div>
 </template>
 
 <script>
 import cryptoRandomString from "crypto-random-string";
+import _find from "lodash/find";
+import _assign from "lodash/assign";
+import _findIndex from "lodash/findIndex";
+import _forEachRight from "lodash/forEachRight";
 import TodoCreator from "./TodoCreator.vue";
 import TodoItem from "./TodoItem.vue";
 
@@ -26,18 +71,51 @@ export default {
   data() {
     return {
       todos: [],
+      filter: "all",
     };
   },
+  computed: {
+    filteredTodos() {
+      switch (this.filter) {
+        case "all":
+        default:
+          return this.todos;
+        case "active": // 해야 할 항목
+          return this.todos.filter((todo) => !todo.done);
+        case "completed": // 완료된 항목
+          return this.todos.filter((todo) => todo.done);
+      }
+    },
+    total() {
+      return this.todos.length;
+    },
+    activeCount() {
+      return this.todos.filter((todo) => !todo.done).length;
+    },
+    completedCount() {
+      return this.total - this.activeCount;
+    },
+    allDone: {
+      get() {
+        return this.total === this.completedCount && this.total > 0;
+      },
+      set(checked) {
+        this.completeAll(checked);
+      },
+    },
+  },
   created() {
-    this.initDB()
+    this.initDB();
   },
   methods: {
     initDB() {
+      // DB에서 값을 가져옴
       const todoString = localStorage.getItem("todo-app");
       const todoObj = JSON.parse(todoString);
 
-      if(todoObj) {
-        this.todos = todoObj
+      // DB에 값이 없다면 안가져오고 새로 만듬
+      if (todoObj) {
+        this.todos = todoObj;
       } else {
         const todosString = JSON.stringify(this.todos);
         localStorage.setItem("todo-app", todosString);
@@ -52,16 +130,83 @@ export default {
         done: false,
       };
 
+      // Create Client
       this.todos.push(newTodo);
+
+      // Create DB
       const todosString = JSON.stringify(this.todos);
       localStorage.setItem("todo-app", todosString);
     },
-    updateTodo() {
-      console.log('updateTodo!')
+    updateTodo(todo, value) {
+      // Create Client(JavaScript 메소드 활용)
+      // const foundTodo = this.todos.find((item=>item.id === todo.id))
+      // Object.assign(foundTodo, value)
+
+      // Create Client(lodash API)
+      const foundTodo = _find(this.todos, { id: todo.id });
+      _assign(foundTodo, value);
+
+      // Create DB
+      const todosString = JSON.stringify(this.todos);
+      localStorage.setItem("todo-app", todosString);
     },
-    deleteTodo() {
-      console.log('deleteTodo!')
-    }
+    deleteTodo(todo) {
+      // Remove Client(lodash는 삭제에 반응성을 가지지 않으므로 Vue의 함수 사용)
+      const foundIndex = _findIndex(this.todos, { id: todo.id });
+      // this.$delete(this.todos, foundIndex) // Vue 2.0에서만 작동
+      delete this.todos[foundIndex]; // Vue 3.0에서 사용
+      this.todos.splice(foundIndex, 1); // Vue3의 Delete는 배열의 요소를 지워주지 않으므로 Splice 함수를 사용함
+
+      // Remove DB
+      const todosString = JSON.stringify(this.todos);
+      localStorage.setItem("todo-app", todosString);
+    },
+    changeFilter(filter) {
+      this.filter = filter;
+    },
+    completeAll(checked) {
+      // done Client
+      this.todos.forEach((todo) => {
+        todo.done = checked;
+      });
+
+      // done DB
+      const todosString = JSON.stringify(this.todos);
+      localStorage.setItem("todo-app", todosString);
+    },
+    clearCompleted() {
+      // 인덱스를 앞에서 부터 삭제하면 인덱스 갱신에 의해 제대로 삭제되지 않을 수 있음
+      // this.todos.forEach(todo => {
+      //   if(todo.done) {
+      //     this.deleteTodo(todo)
+      //   }
+      // })
+
+      // javascrip 함수를 사용한 인덱스를 뒤에서부터 삭제
+      // this.todos.reduce((list, todo, index) => {
+      //   if(todo.done) {
+      //     list.push(index)
+      //   }
+      //   return list
+      // }, [])
+      // .reverse()
+      // .forEach(index => {
+      //   this.deleteTodo(this.todos[index])
+      // })
+
+      // lodash를 사용한 API
+      _forEachRight(this.todos, (todo) => {
+        if (todo.done) {
+          this.deleteTodo(todo);
+        }
+      });
+    },
   },
 };
 </script>
+
+<style scoped lang="scss">
+button.active {
+  font-weight: bold;
+}
+</style>
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,72 +1,130 @@
 <template>
-  <div class="todo-item">
-    <input 
-        v-model="done" 
-        type="checkbox" 
-    />
-
-    <div class="item__title-wrap">
-      <div class="item__title">
-        {{ todo.title }}
-      </div>
-      <div class="item__date">
-        {{ date }}
+  <div 
+    :class="{ done }"
+    class="todo-item">
+    <div v-if="isEditMode" class="item__inner item--edit">
+      <!-- 
+        ref는 script 부분에서 해당 요소를 가져오기 쉽게하기 위함
+        @input은 값이 입력됬을 경우 editedTitle의 값을 변경해주기 위함
+        @keypress는 이름 그대로 키가 눌렸을 경우 발생하는 이벤트 메소드를 설정
+      -->
+      <input 
+        ref="titleInput"
+        :value="editedTitle"
+        type="text"
+        @input="editedTitle = $event.target.value"
+        @keypress.enter="editedTodo"
+        @keypress.esc="offEditMode"
+      >
+      <!-- key를 구분해줘야 아래의 수정/삭제 버튼과 별개의 요소로 구분됨 -->
+      <div class="item__actions">
+        <button key="complete" @click="editedTodo">완료</button>
+        <button key="cancle" @click="offEditMode">취소</button>
       </div>
     </div>
-
-    <div class="item__actions">
-      <button @click="onEditMode">수정</button>
-      <button @click="deleteTodo">삭제</button>
+    <div v-else class="item__inner item--normal">
+       <input v-model="done" type="checkbox" />
+      <div class="item__title-wrap">
+        <div class="item__title">
+          {{ todo.title }}
+        </div>
+        <div class="item__date">
+          {{ date }}
+        </div>
+      </div>
+      <div class="item__actions">
+        <button key="update" @click="onEditMode">수정</button>
+        <button key="delete" @click="deleteTodo">삭제</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import dayjs from 'dayjs'
+import dayjs from "dayjs";
 
 export default {
   props: {
+    // 받는 props의 형식을 지정해줌
     todo_props: Object,
   },
   data() {
     return {
       todo: this.todo_props,
+      isEditMode: false,
+      editedTitle: ''
     };
   },
   computed: {
     done: {
       get() {
-          return this.todo.done
+        return this.todo.done;
       },
       set(done) {
-          this.updateTodo({
-              done
-          })
+        this.updateTodo({
+          done,
+        });
       },
     },
-    date () {
-        const date = dayjs(this.todo.createdAt)
-        const isSame = date.isSame(this.todo.updatedAt)
+    date() {
+      const date = dayjs(this.todo.createdAt);
+      const isSame = date.isSame(this.todo.updatedAt);
 
-        if(isSame) {
-            return date.format('YYYY년 MM월 DD일')
-        } else {
-            return `${date.format('YYYY년 MM월 DD일')} (edited)`
-        }
-        
-    }
+      if (isSame) {
+        return date.format("YYYY년 MM월 DD일");
+      } else {
+        return `${date.format("YYYY년 MM월 DD일")} (edited)`;
+      }
+    },
   },
   methods: {
-    onEditMode() {},
-    offEditMode() {
+    editedTodo() {
+      if(this.todo.title !== this.editedTitle) {
+        this.updateTodo({
+          title: this.editedTitle,
+          updatedAt: new Date()
+        })
+      }
 
+      this.offEditMode()
+    },
+    onEditMode() {
+      this.isEditMode = true
+      this.editedTitle = this.todo.title
+
+      // v-if로 바인딩되는 요소를 정했기 때문에 위의 this.isEditMode를 바꾸면 바인딩되는 요소가 바뀌는 중이므로
+      // focus할 요소를 찾지 못하는 경우가 발생할 수 있음, 따라서 $nectTick을 사용하면 요소가 바인딩되는것을 기다림
+      this.$nextTick (() => {
+        this.$refs.titleInput.focus()
+      })
+    },
+    offEditMode() {
+      this.isEditMode = false
     },
     updateTodo(value) {
-        this.$emit('update-todo', this.todo, value)
+      this.$emit("update-todo", this.todo, value);
     },
     deleteTodo() {
-        this.$emit('delete-todo', this.todo)
+      this.$emit("delete-todo", this.todo);
     },
   },
 };
 </script>
+
+
+<style scoped lang="scss">
+  .todo-item {
+    margin-bottom: 10px;
+    .item__inner {
+      display: flex;
+    }
+    .item__date {
+      font-size: 12px;
+    }
+    &.done {
+      .item__title {
+        text-decoration: line-through;
+      }
+    }
+  }
+</style>
